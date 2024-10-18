329a897459e9d1fdb627b0b48eff0d2fad697685
rustem <temirgalirustem05@gmail.com>
2024-10-18 16:56:33 +0500

    upd users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', verbose_name='profile')
+    avatar = models.ImageField(upload_to='avatars/', verbose_name='avatar', null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True, verbose_name='birth date')
+    number = models.CharField(max_length=20, null=True, blank=True, verbose_name='number')
+    resume = models.FileField(upload_to='resumes/', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user}'
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+
 from django.shortcuts import render
 
 # Create your views here.
+
+@login_required
+def login_view(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            next = request.POST.get('next')
+            if next:
+                return redirect(next)
+            return redirect('index')
+        else:
+            context['error'] = True
+        return render(request, '#template-name')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    next = request.GET.get('next')
+    if next:
+        return redirect(next)
+    return redirect('index')
+
